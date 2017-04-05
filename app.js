3f84d5536d32f1d2c90c5a25d33273eb127a28b9
jtkiesel <rstudio@example.com>
2017-04-05 16:08:09 -0400

    Added app.

---
+++ app.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+	console.log('I am ready!');
+});
+
+client.on('message', message => {
+	if (message.content === 'ping') {
+		message.reply('pong');
+	}
+});
+
+client.login('Mjk5MjczNjQ1MjE2MzY2NTky.C8bf0A.2xG6kiAxG569srFSvWqKQBhQHIM');
