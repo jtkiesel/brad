b4bf689edd2dfdb00b53d244965d9a3b9898b440
jtkiesel <rstudio@example.com>
2017-04-09 15:26:22 -0400

    Got rid of role IDs, and added recursive function to remove all roles from a member.

--- app.js
+++ app.js
@@ -1,34 +1,45 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-//const request = require('request');
+const request = require('request');
 const fs = require('fs');
 
-var roleIds = {'Science': '298695769848938496', 'Technology': '298696545249918976', 'Research': '298696653937049600', 'Engineering': '298696666536607745', 'Arts': '298696704419430402', 'Math': '298696720538402816', 'Spirit': '298696736652656640', 'Opportunity': '298696748111757326', 'Design': '298696794056032257', 'Innovate': '298696806806716418', 'Non-Competitor': '298700344110612480'};
+var roles = ['Science', 'Technology', 'Research', 'Engineering', 'Arts', 'Math', 'Spirit', 'Opportunity', 'Design', 'Innovate', 'Non-Competitor'];
 var divisions = {};
 
-fs.readFile(__dirname + '/divisions.csv', 'utf8', (err, data) => {
-	if (err) {
-		throw err;
-	}
-	var teams = data.split('\r\n');
+function updateDivisions() {
+	fs.readFile(__dirname + '/divisions.csv', 'utf8', (err, data) => {
+		if (err) {
+			throw err;
+		}
+		var teams = data.split('\r\n');
 
-	for (var i = 0; i < teams.length; i++) {
-		var team = teams[i].split(',');
-		var teamId = team[0];
-		var division = team[1];
-		divisions[teamId] = division;
+		for (var i = 0; i < teams.length; i++) {
+			var team = teams[i].split(',');
+			var teamId = team[0];
+			var division = team[1];
+			divisions[teamId] = division;
+		}
+	});
+}
+
+function removeRoles(member, roleNames) {
+	if (roleNames.length > 0) {
+		member.removeRole(member.guild.roles.find('name', roleNames.shift())).then(() => {
+			removeRoles(member, roleNames);
+		}).catch(console.log);
 	}
-});
+}
 
 function setDivision(member, nickname) {
 	var teamId = nickname.split(' | ')[1];
 	var division = divisions[teamId];
 
-	if (Object.keys(roleIds).indexOf(division) === -1) {
+	if (roles.indexOf(division) === -1) {
 		division = 'Non-Competitor';
 	}
-	member.removeRole(member.guild.roles.find('name', 'Science')).then(() => {
+	removeRoles(member, roles);
+/*	member.removeRole(member.guild.roles.find('name', 'Science')).then(() => {
 		member.removeRole(member.guild.roles.find('name', 'Technology')).then(() => {
 			member.removeRole(member.guild.roles.find('name', 'Research')).then(() => {
 				member.removeRole(member.guild.roles.find('name', 'Engineering')).then(() => {
@@ -50,7 +61,7 @@ function setDivision(member, nickname) {
 				}).catch(console.log);
 			}).catch(console.log);
 		}).catch(console.log);
-	}).catch(console.log);
+	}).catch(console.log);*/
 }
 
 client.on('ready', () => {
@@ -59,19 +70,35 @@ client.on('ready', () => {
 
 client.on('message', message => {
 	if (message.channel.name === 'verify' && message.author != client.user) {
-		var nickname = message.content.replace(/\s+/g, '').split('|');
+		var nickname = message.content.split('|');
 
 		if (nickname.length == 2) {
-			var name = nickname[0];
-			var teamId = nickname[1].toUpperCase();
+			var name = nickname[0].trim();
+			var teamId = nickname[1].trim().toUpperCase();
 
 			if (/^([0-9]{1,5}[A-Z]?|[A-Z]{2,6}[0-9]{0,2})$/.test(teamId)) {
-				nickname = name + ' | ' + teamId;
-				message.member.setNickname(nickname);
-				setDivision(message.member, nickname);
+				request('https://api.vexdb.io/v1/get_teams?apike=shNhxcphXlIXQVE2Npeu&team=' + teamId, (error, response, body) => {
+					body = JSON.parse(body);
+
+					if (body.status == 1) {
+						if (body.size > 0) {
+							nickname = name + ' | ' + teamId;
+							message.member.setNickname(nickname);
+							setDivision(message.member, nickname);
+						} else {
+							message.reply('That team ID does not exist.');
+						}
+					} else {
+						message.reply('Sorry, VexDB messed up (Nathan pls).');
+					}
+				}
+			} else {
+				message.reply('Please enter a valid team ID (example: 1234A, or ABC1).');
 			}
 		}
 	}
 });
+
+updateDivisions();
 
 client.login('Bot Mjk5MjczNjQ1MjE2MzY2NTky.C8bf0A.2xG6kiAxG569srFSvWqKQBhQHIM');
