83ce629f8783b5ebf4ef955abc7058c8c3cd3078
jtkiesel <rstudio@example.com>
2017-04-09 19:22:04 -0400

    Fixed syntax error.

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ client.on('message', message => {
 				message.reply('Please enter a valid team ID (example: ***1234A***, or ***ABC1***).');
 			}
 		} else {
-			message.reply('Incorrect format. Separate name and team ID by a single "|" (example: ***Jordan | 1234A***).
+			message.reply('Incorrect format. Separate name and team ID by a single "|" (example: ***Jordan | 1234A***).');
 		}
 	}
 });
