aedff51529dd163aa542ed900528dfe50d26414b
jtkiesel <rstudio@example.com>
2017-04-09 22:13:35 -0400

    Debugging update.

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ function setDivision(member, nickname) {
 	if (roles.indexOf(division) === -1) {
 		division = 'Non-Competitor';
 	}
-	removeRoles(member, roles.slice(roles.indexOf(division), 1));
+	removeRoles(member, roles.slice().splice(roles.indexOf(division), 1));
 	member.addRole(member.guild.roles.find('name', division));
 }
 
